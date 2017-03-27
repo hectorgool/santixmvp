7b42439796010ff96ec1d367c7dc3c4eb9ca0c08
Hector Gonzalez <hector.gonzalez@enova.mx>
2017-03-27 10:02:56 -0600

    add modals

--- db/migrate/20170306235517_create_tours.rb
+++ db/migrate/20170306235517_create_tours.rb
@@ -2,7 +2,7 @@ class CreateTours < ActiveRecord::Migration[5.0]
   def change
     create_table :tours do |t|
       t.string :name
-      t.string :description
+      t.text :description
 
       t.timestamps
     end
