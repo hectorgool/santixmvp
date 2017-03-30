6db7277c0e622f1faf3dab30d57f689143da82de
hectorgool <hectorgool@gmail.com>
2017-03-30 08:56:21 -0600

    install facebook auth

--- app/assets/javascripts/tours.coffee
+++ app/assets/javascripts/tours.coffee
@@ -6,4 +6,3 @@ $ ->
 	$("#add_file").on "ajax:success", (event, data) ->
 		$("#attachments").append data
 		$(this).data "params", { index: $("#attachments div.file").length }
-
--- config/routes.rb
+++ config/routes.rb
@@ -51,6 +51,6 @@ Rails.application.routes.draw do
     resources :reservations
     #resources :comments
   end
-
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
